fea66d3dc4c968f1fa85921968c7ce35fe4184ea
jianan <yangjianan@diynova.com>
2022-11-23 18:21:43 +0800

    style：delete unnecessary import

--- contracts/evt-factory/Multimedia/Multimedia.sol
+++ contracts/evt-factory/Multimedia/Multimedia.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
